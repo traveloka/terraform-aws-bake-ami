bfa5b0da9521d65ebbebbd7bff614471e6b684b7
Salvian Reynaldi <salvian@traveloka.com>
2018-02-08 15:34:44 +0700

    output the instance profile name

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "bake-ami-pipeline-input" {
     value = "s3://${var.service-s3-bucket}/${local.bake-pipeline-input-key}"
     description = "where to store the zip file for the ami baking pipeline"
 }
+
+output "template-instance-profile" {
+    value = "${aws_iam_instance_profile.template.name}"
+    description = "the name of the instance profile created by this module. This should used by the baking AMI instances"
+}
