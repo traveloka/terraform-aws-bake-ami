69029dff3456db65021bf3cd3af5687caa061147
Salvian Reynaldi <salvian@traveloka.com>
2018-04-10 14:39:09 +0700

    fix typo

--- local-variables.tf
+++ local-variables.tf
@@ -1,4 +1,4 @@
 locals {
   bake-pipeline-name = "${var.service-name}-bake-ami"
-  s3-bucket-name     = "${format("%s-%s-%s-%s", var.service-name,"codebuild-cache", data.aws_caller_identity.current.account_id, random_id.s3-bucket-suffix.hex}"
+  s3-bucket-name     = "${format("%s-%s-%s-%s", var.service-name,"codebuild-cache", data.aws_caller_identity.current.account_id, random_id.s3-bucket-suffix.hex)}"
 }
